a63ef4f6481013ef1bf64d2b59534b4f60a0a829
vcokltfre <vcokltfre@gmail.com>
2021-05-19 12:41:07 +0100

    feat: add database

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ if not getenv("USE_DOCKER_ENV"):
     load_dotenv()
 
 from src.routers import router
+from src.utils.database import Database
+
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.include_router(router)
+
+db = Database()
+
+@app.on_event("startup")
+async def on_startup() -> None:
+    """Initialise the database."""
+
+    await db.ainit()
