9b16a72f274e610b7360c9ccb4b52b7c0dba72e7
vcokltfre <vcokltfre@gmail.com>
2021-05-19 14:19:33 +0100

    feat: add internal router

--- src/routers/v1/__init__.py
+++ src/routers/v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from .moots import router as moots_router
+from .internal import router as internal_router
 
 
 router = APIRouter(prefix="/v1")
 router.include_router(moots_router)
+router.include_router(internal_router)
