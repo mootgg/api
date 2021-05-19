e95ac6d74f6a9147c6c798895a198d43579869ca
vcokltfre <vcokltfre@gmail.com>
2021-05-19 12:13:10 +0100

    feat: add api routers

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from os import getenv
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-
 if not getenv("USE_DOCKER_ENV"):
     load_dotenv()
 
+from src.routers import router
+
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+app.include_router(router)
